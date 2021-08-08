8b1e04a2b83a5dfc754ede4b647cc5b13937fa41
Georgios Keramidas <georgiosmidas@gmail.com>
2021-08-08 12:50:34 +0100

    Added tests

--- littlepolls/django/littlepolls/polls/tests.py
+++ littlepolls/django/littlepolls/polls/tests.py
@@ -4,6 +4,14 @@ from django.utils import timezone
 from django.urls import reverse
 from .models import Question
 
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionModelTests(TestCase):
 
